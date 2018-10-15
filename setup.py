7a2495b1096a6494f77e9a89e66278ea5370b61a
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-15 15:15:16 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+        name = "rnaseqpipeline",
+        version = "0.1",
+        description = 'WUR nematology rnaseq pipeline',
+        packages = ['rnaseqpipeline'],
+        scripts = ['rnaseqpipeline/rnaseqpipeline']
+        )
