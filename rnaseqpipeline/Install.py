b9aa9457f71378f146f258502ff9a56d192bd20b
Joris <joris.vansteenbrugge@wur.nl>
2018-10-16 11:20:08 +0200

    Indendations and formatting

--- rnaseqpipeline/Install.py
+++ rnaseqpipeline/Install.py
@@ -14,83 +14,83 @@ class Install():
         def RepeatModeler(options):
             print("Installing RepeatModeler")
 
-        def RECON():
-            recon_url = 'http://www.repeatmasker.org/RepeatModeler/RECON-1.08.tar.gz'
-            download_cmd = 'wget {0} -O {1}/recon.tar.gz; cd {1}; tar xf recon.tar.gz;'.format(
-            recon_url, options.install_dir
-            )
-            # Download and extract
-            sp.call(download_cmd, shell=True, stdout=FNULL)
+            def RECON():
+                recon_url = 'http://www.repeatmasker.org/RepeatModeler/RECON-1.08.tar.gz'
+                download_cmd = 'wget {0} -O {1}/recon.tar.gz; cd {1}; tar xf recon.tar.gz;'.format(
+                recon_url, options.install_dir
+                )
+                # Download and extract
+                sp.call(download_cmd, shell=True, stdout=FNULL)
 
-            # Building
-            sp.call('cd {}/RECON-1.08/src; make; make install'.format(options.install_dir), shell = True)
-            # Modify the REcon scrip to use the right paths
-            sed_cmd = "sed -i 's+$path = \"\";+$path = {0}/RECON-1.08/bin+g' {0}/RECON-1.08/scripts/recon.pl".format(
-            options.install_dir
-            )
-            sp.call(sed_cmd, shell=True, stdout=FNULL)
+                # Building
+                sp.call('cd {}/RECON-1.08/src; make; make install'.format(options.install_dir), shell = True)
+                # Modify the REcon scrip to use the right paths
+                sed_cmd = "sed -i 's+$path = \"\";+$path = {0}/RECON-1.08/bin+g' {0}/RECON-1.08/scripts/recon.pl".format(
+                options.install_dir
+                )
+                sp.call(sed_cmd, shell=True, stdout=FNULL)
+                    # Cleanup
+                sp.call('rm {}/recon.tar.gz'.format(options.install_dir), shell=True, stdout=FNULL)
+
+            def RepeatScout():
+                recon_url = 'http://www.repeatmasker.org/RepeatScout-1.0.5.tar.gz'
+                download_cmd = 'wget {0} -O {1}/RepeatScout.tar.gz; cd {1}; tar xf RepeatScout.tar.gz;'.format(
+                recon_url, options.install_dir
+                )
+                # Download and extract
+                sp.call(download_cmd, shell=True, stdout=FNULL)
+                # Building
+                sp.call('cd {}/RepeatScout-1/ ; make'.format(options.install_dir),
+                shell = True, stdout=FNULL)
+
                 # Cleanup
-            sp.call('rm {}/recon.tar.gz'.format(options.install_dir), shell=True, stdout=FNULL)
+                sp.call('rm {}/RepeatScout.tar.gz'.format(options.install_dir),
+                shell=True, stdout=FNULL)
+                bashrc = "echo export PATH=$PATH:{}/RepeatScout-1/ >> ~/.bashrc".format(options.install_dir)
+                sp.call(bashrc, shell = True, stdout=FNULL)
 
-        def RepeatScout():
-            recon_url = 'http://www.repeatmasker.org/RepeatScout-1.0.5.tar.gz'
-            download_cmd = 'wget {0} -O {1}/RepeatScout.tar.gz; cd {1}; tar xf RepeatScout.tar.gz;'.format(
-            recon_url, options.install_dir
-            )
-            # Download and extract
-            sp.call(download_cmd, shell=True, stdout=FNULL)
-            # Building
-            sp.call('cd {}/RepeatScout-1/ ; make'.format(options.install_dir),
-            shell = True, stdout=FNULL)
+            def TandenRepeatFinder():
+                conda_channel = "conda config --add channels {}"
+                sp.call(conda_channel.format('bioconda'),
+                        shell = True, stdout=FNULL)
+                sp.call(conda_channel.format('conda-forge'),
+                        shell = True, stdout=FNULL)
+                sp.call(conda_channel.format('WURnematology'),
+                        shell = True, stdout=FNULL)
+                sp.call("conda install -y tandemrepeatfinder", 
+                        shell = True, stdout=FNULL)
 
-            # Cleanup
-            sp.call('rm {}/RepeatScout.tar.gz'.format(options.install_dir),
-            shell=True, stdout=FNULL)
-            bashrc = "echo export PATH=$PATH:{}/RepeatScout-1/ >> ~/.bashrc".format(options.install_dir)
-            sp.call(bashrc, shell = True, stdout=FNULL)
+            def RMBlast():
 
-        def TandenRepeatFinder():
-            conda_channel = "conda config --add channels {}"
-            sp.call(conda_channel.format('bioconda'),
+                cmd = "wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.6.0/ncbi-blast-2.6.0+-src.tar.gz -O {}/ncbi-blast.tar.gz".format(
+                    options.install_dir)
+                sp.call(cmd, 
                     shell = True, stdout=FNULL)
-            sp.call(conda_channel.format('conda-forge'),
+                sp.call('tar xf ncbi-blast.tar.gz', 
                     shell = True, stdout=FNULL)
-            sp.call(conda_channel.format('WURnematology'),
+                sp.call('wget http://www.repeatmasker.org/isb-2.6.0+-changes-vers2.patch.gz -O {}/isb-2.6.0+-changes-vers2.patch.gz'.format(
+                options.install_dir), 
                     shell = True, stdout=FNULL)
-            sp.call("conda install -y tandemrepeatfinder", 
+                sp.call('gunzip {}/isb-2.6.0+-changes-vers2.patch.gz'.format(options.install_dir),
                     shell = True, stdout=FNULL)
-
-        def RMBlast():
-
-            cmd = "wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.6.0/ncbi-blast-2.6.0+-src.tar.gz -O {}/ncbi-blast.tar.gz".format(
-                options.install_dir)
-            sp.call(cmd, 
-                shell = True, stdout=FNULL)
-            sp.call('tar xf ncbi-blast.tar.gz', 
-                shell = True, stdout=FNULL)
-            sp.call('wget http://www.repeatmasker.org/isb-2.6.0+-changes-vers2.patch.gz -O {}/isb-2.6.0+-changes-vers2.patch.gz'.format(
-            options.install_dir), 
-                shell = True, stdout=FNULL)
-            sp.call('gunzip {}/isb-2.6.0+-changes-vers2.patch.gz'.format(options.install_dir),
-                shell = True, stdout=FNULL)
-            sp.call("cd {}/ncbi-blast-2.6.0+-src ; patch -p1 < ../isb-2.6.0+-changes-vers2.patch".format(options.install_dir),
-                shell = True, stdout=FNULL)
-            sp.call('cd {0}/ncbi-blast-2.6.0+-src/c++; ./configure --with-mt --prefix={0}/ncbi-blast-2.6.0+-src/ --without-debug'.format(
-            options.install_dir), 
+                sp.call("cd {}/ncbi-blast-2.6.0+-src ; patch -p1 < ../isb-2.6.0+-changes-vers2.patch".format(options.install_dir),
                     shell = True, stdout=FNULL)
-            sp.call('cd {0}/ncbi-blast-2.6.0+-src/c++; make; make install'.format(options.install_dir), 
-                    shell = True, stdout=FNULL)
-        def RepeatMasker():
-            sp.call('wget -c http://www.repeatmasker.org/RepeatMasker-open-4-0-7.tar.gz -O {}/RepeatMasker-open-4-0-7.tar.gz'.format(
-                    options.install_dir), 
-                    shell = True, stdout=FNULL)
-            sp.call('cd {}; tar xf RepeatMasker-open-4-0-7.tar.gz'.format(options.install_dir), 
-                    shell = True, stdout=FNULL)
-            sp.call('cd RepeatMasker; ./configure --prefix=$(pwd)', 
-                    shell = True, stdout=FNULL)
-        RECON()
-        RepeatScout()
-        TandenRepeatFinder()
-        RMBlast()
-        RepeatMasker()
+                sp.call('cd {0}/ncbi-blast-2.6.0+-src/c++; ./configure --with-mt --prefix={0}/ncbi-blast-2.6.0+-src/ --without-debug'.format(
+                options.install_dir), 
+                        shell = True, stdout=FNULL)
+                sp.call('cd {0}/ncbi-blast-2.6.0+-src/c++; make; make install'.format(options.install_dir), 
+                        shell = True, stdout=FNULL)
+            def RepeatMasker():
+                sp.call('wget -c http://www.repeatmasker.org/RepeatMasker-open-4-0-7.tar.gz -O {}/RepeatMasker-open-4-0-7.tar.gz'.format(
+                        options.install_dir), 
+                        shell = True, stdout=FNULL)
+                sp.call('cd {}; tar xf RepeatMasker-open-4-0-7.tar.gz'.format(options.install_dir), 
+                        shell = True, stdout=FNULL)
+                sp.call('cd RepeatMasker; ./configure --prefix=$(pwd)', 
+                        shell = True, stdout=FNULL)
+            RECON()
+            RepeatScout()
+            TandenRepeatFinder()
+            RMBlast()
+            RepeatMasker()
         RepeatModeler(options)
