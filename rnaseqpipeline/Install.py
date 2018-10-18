6dc1568f6b3fe86a3be3eb08afdfa4ccc7efe1d0
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-18 13:40:37 +0200

    Writing stdout and stder to log files

--- rnaseqpipeline/Install.py
+++ rnaseqpipeline/Install.py
@@ -11,11 +11,11 @@ class Install():
 
     def perform_installation(options):
         import subprocess as sp
-        import os
 
         print('Performing installation in {} '.format(options.install_dir))
 
-        FNULL = open(os.devnull, 'w')
+        out_file = open("{}/out.log".format(options.install_dir), 'w')
+        err_file = open("{}/err.log".format(options.install_dir), 'w')
 
         def RepeatModeler(options):
             print_pass("Installing RepeatModeler")
@@ -32,22 +32,24 @@ class Install():
                 recon_url, options.install_dir
                 )
                 # Download and extract
-                sp.call(download_cmd, shell=True, stdout=FNULL)
+                sp.call(download_cmd, shell=True, stdout=out_file, stderr = err_file)
 
                 # Building
-                sp.call('cd {}/RECON-1.08/src; make; make install'.format(options.install_dir), shell = True)
+                sp.call('cd {}/RECON-1.08/src; make; make install'.format(options.install_dir), shell = True,
+                     stdout=out_file, stderr = err_file)
                 # Modify the REcon scrip to use the right paths
                 sed_cmd = "sed -i 's+$path = \"\";+$path = {0}/RECON-1.08/bin+g' {0}/RECON-1.08/scripts/recon.pl".format(
-                options.install_dir
-                )
-                sp.call(sed_cmd, shell=True, stdout=FNULL)
+                    options.install_dir)
+
+                sp.call(sed_cmd,
+                    shell=True,  stdout=out_file, stderr = err_file)
                     # Cleanup
-                sp.call('rm {}/recon.tar.gz'.format(options.install_dir), shell=True, stdout=FNULL)
+                sp.call('rm {}/recon.tar.gz'.format(options.install_dir),
+                    shell=True, stdout=out_file, stderr = err_file)
 
                 #Add files to path
                 sp.call("echo \'# RECON-1.08 installation dir\' >> ~/.bashrc; echo \'export PATH=$PATH:{0}/RECON-1.08/bin\' >> ~/.bashrc".format(options.install_dir),
-                    shell = True,
-                    stdout = FNULL)
+                    shell = True,  stdout=out_file, stderr = err_file)
 
             def RepeatScout():
                 if verify_installation('build_lmer_table', "Usage"):
@@ -58,23 +60,23 @@ class Install():
 
                 recon_url = 'http://www.repeatmasker.org/RepeatScout-1.0.5.tar.gz'
                 download_cmd = 'wget {0} -O {1}/RepeatScout.tar.gz; cd {1}; tar xf RepeatScout.tar.gz;'.format(
-                    recon_url, options.install_dir
-                )
+                    recon_url, options.install_dir )
+
                 # Download and extract
                 sp.call(download_cmd,
-                    shell = True, stdout = FNULL, stderr = FNULL)
+                    shell = True,  stdout=out_file, stderr = err_file)
                 # Building
                 sp.call('cd {}/RepeatScout-1/ ; make'.format(options.install_dir),
-                    shell = True, stdout = FNULL)
+                    shell = True,  stdout=out_file, stderr = err_file)
 
                 # Cleanup
                 sp.call('rm {}/RepeatScout.tar.gz'.format(options.install_dir),
-                    shell=True, stdout = FNULL)
+                    shell=True,  stdout=out_file, stderr = err_file)
 
                 bashrc = "echo \'# RepeatScout 1.0.5 installation dir\'; echo \'export PATH=$PATH:{}/RepeatScout-1/ \' >> ~/.bashrc".format(options.install_dir)
 
                 sp.call(bashrc,
-                    shell = True, stdout = FNULL)
+                    shell = True,  stdout=out_file, stderr = err_file)
 
             def TandenRepeatFinder():
                 if verify_installation('trf409.linux64', 'Please use:'):
@@ -83,13 +85,13 @@ class Install():
 
                 conda_channel = "conda config --add channels {}"
                 sp.call(conda_channel.format('bioconda'),
-                        shell = True, stdout=FNULL)
+                        shell = True,  stdout=out_file, stderr = err_file)
                 sp.call(conda_channel.format('conda-forge'),
-                        shell = True, stdout=FNULL)
+                        shell = True,  stdout=out_file, stderr = err_file)
                 sp.call(conda_channel.format('WURnematology'),
-                        shell = True, stdout=FNULL)
+                        shell = True,  stdout=out_file, stderr = err_file)
                 sp.call("conda install -y tandemrepeatfinder",
-                        shell = True, stdout=FNULL)
+                        shell = True,  stdout=out_file, stderr = err_file)
 
                 print("    TendemRepeatFinder installation complete")
 
@@ -105,28 +107,28 @@ class Install():
                 cmd = "wget -c ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.6.0/ncbi-blast-2.6.0+-src.tar.gz -O {}/ncbi-blast.tar.gz".format(
                     options.install_dir)
                 sp.call(cmd,
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
                 sp.call('cd {}; tar xf ncbi-blast.tar.gz'.format(options.install_dir),
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
                 sp.call('wget -c http://www.repeatmasker.org/isb-2.6.0+-changes-vers2.patch.gz -O {}/isb-2.6.0+-changes-vers2.patch.gz'.format(
                 options.install_dir),
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
                 sp.call('gunzip -f {}/isb-2.6.0+-changes-vers2.patch.gz'.format(options.install_dir),
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
                 sp.call("cd {}/ncbi-blast-2.6.0+-src ; patch -p1 < ../isb-2.6.0+-changes-vers2.patch".format(options.install_dir),
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
                 sp.call('cd {0}/ncbi-blast-2.6.0+-src/c++; ./configure --with-mt --prefix={0}/ncbi-blast-2.6.0+-src/ --without-debug'.format(
                   options.install_dir),
-                       shell = True)
+                       shell = True,  stdout=out_file, stderr = err_file)
                 print("        compiling ncbi blast (this takes at least an hour)")
                 sp.call('cd {0}/ncbi-blast-2.6.0+-src/c++; make; make install'.format(options.install_dir),
-                       shell = True)
+                       shell = True,  stdout=out_file, stderr = err_file)
 
                 path = "{0}/ncbi-blast-2.6.0+-src/bin".format(options.install_dir)
 
                 sp.call("echo \'# RMBlast installation dir\' >> ~/.bashrc; echo \'export PATH={}:$PATH\' >> ~/.bashrc".format(
                     path
-                ), shell = True)
+                ), shell = True,  stdout=out_file, stderr = err_file)
 
 
             def RepeatMasker():
@@ -138,9 +140,9 @@ class Install():
                 print_pass("    Installing RepeatMasker")
                 sp.call('wget -c http://www.repeatmasker.org/RepeatMasker-open-4-0-7.tar.gz -O {}/RepeatMasker-open-4-0-7.tar.gz'.format(
                         options.install_dir),
-                        shell = True, stdout=FNULL, stderr = FNULL)
+                        shell = True,  stdout=out_file, stderr = err_file)
                 sp.call('cd {}; tar xf RepeatMasker-open-4-0-7.tar.gz'.format(options.install_dir),
-                        shell = True, stdout=FNULL)
+                        shell = True,  stdout=out_file, stderr = err_file)
 
                 # By default, the configure script requires manual input for different configuration steps,
                 # This is annoying in a headless installation (such as this one) therefore I modified the original
@@ -148,21 +150,21 @@ class Install():
                 # Download that now:
                 sp.call(["wget", "http://www.bioinformatics.nl/~steen176/repeatmask_config", # Rreplace with actual URL
                 "-O", "{}/RepeatMasker_CONFIG".format(options.install_dir)
-                        ], stdout = FNULL)
+                        ],  stdout=out_file, stderr = err_file)
 
                 # Now we need to update all the paths required relative to the installation directory
                 repeat_mask_cmd = "sed -i 's+ACTUALINSTALLDIR+{0}+g' {0}/RepeatMasker_CONFIG".format(
                     options.install_dir
                 )
-                sp.call(repeat_mask_cmd, shell = True)
+                sp.call(repeat_mask_cmd, shell = True,  stdout=out_file, stderr = err_file)
 
                 sp.call('cd {}/RepeatMasker;cp ../RepeatMasker_CONFIG ./configure; perl configure '.format(options.install_dir),
-                        shell = True)
+                        shell = True,  stdout=out_file, stderr = err_file)
 
                 sp.call("echo \'# RepeatMasker install dir\' >> ~/.bashrc ; echo \'export PATH={}/RepeatMasker:$PATH\' >> ~/.bashrc".format(
                     options.install_dir
                 ),
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
 
 
                 sp.call("wget -O- http://cpanmin.us | perl - -l ~/perl5 App::cpanminus local::lib", shell = True)
@@ -178,12 +180,12 @@ class Install():
                     return
 
                 sp.call("mkdir {0}/nseg; cd {0}/nseg; wget ftp://ftp.ncbi.nih.gov/pub/seg/nseg/*".format(options.install_dir),
-                    shell = True)
-                sp.call("cd {}/nseg; make".format(options.install_dir), shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
+                sp.call("cd {}/nseg; make".format(options.install_dir), shell = True,  stdout=out_file, stderr = err_file)
                 sp.call("echo \'# NSEG installation dir\' >> ~/.bashrc; echo \'export PATH={}/nseg:$PATH\' >> ~/.bashrc".format(
                     options.install_dir
                 ),
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
 
             RECON()
             RepeatScout()
@@ -199,13 +201,13 @@ class Install():
             if verify_installation('RepeatModeler', " RepeatModeler - Model repetitive DNA"):
                 print_pass("    RepeatModeler Already Installed")
                 return
-                
-            print("INSTALL ACTUAL REPEATMODELER")
+
+
             sp.call("wget -c http://www.repeatmasker.org/RepeatModeler/RepeatModeler-open-1.0.11.tar.gz -O {}/RepeatModeler-open-1.0.11.tar.gz".format(options.install_dir),
-                shell = True, stdout = FNULL)
+                shell = True,  stdout=out_file, stderr = err_file)
 
             sp.call('cd {}; tar xf RepeatModeler-open-1.0.11.tar.gz'.format(options.install_dir),
-                    shell = True, stdout = FNULL)
+                    shell = True,  stdout=out_file, stderr = err_file)
 
             # By default, the configure script requires manual input for different configuration steps,
             # This is annoying in a headless installation (such as this one) therefore I modified the original
@@ -213,24 +215,24 @@ class Install():
             # Download that now:
             sp.call(["wget", "http://www.bioinformatics.nl/~steen176/repeatmodeler_config", # Rreplace with actual URL
             "-O", "{}/RepeatModeler_CONFIG".format(options.install_dir)
-                    ], stdout = FNULL)
+                    ],  stdout=out_file, stderr = err_file)
 
             # Now we need to update all the paths required relative to the installation directory
             repeat_mask_cmd = "sed -i 's+ACTUALINSTALLDIR+{0}+g' {0}/RepeatModeler_CONFIG; sed -i \"s+TRFBINLOCATION+$(which trf409.linux64)+g\" {0}/RepeatModeler_CONFIG".format(
                 options.install_dir
             )
-            sp.call(repeat_mask_cmd, shell = True)
+            sp.call(repeat_mask_cmd, shell = True,  stdout=out_file, stderr = err_file)
 
             sp.call('cd {}/RepeatModeler-open-1.0.11;cp ../RepeatModeler_CONFIG RepModelConfig.pm'.format(options.install_dir),
-                    shell = True)
+                    shell = True,  stdout=out_file, stderr = err_file)
 
             sp.call('sed -i "s,\#\!/u1/local/bin/perl,\#\!$(which perl),g" {}/RepeatModeler-open-1.0.11/RepeatModeler'.format(options.install_dir),
-                    shell = True) # replace the perl shebang line
+                    shell = True,  stdout=out_file, stderr = err_file) # replace the perl shebang line
 
             sp.call("echo \'# RepeatModeler installation dir\' >> ~/.bashrc; echo \'export PATH={}/RepeatModeler-open-1.0.11:$PATH\' >> ~/.bashrc".format(
                 options.install_dir
             ),
-                shell = True)
+                shell = True,  stdout=out_file, stderr = err_file)
         RepeatModeler(options)
 
 def verify_installation(command, required_out):
