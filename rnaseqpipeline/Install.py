4d87ba42bb3617f72b7ff196cf81209d57fa1f31
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-18 13:23:20 +0200

    fixed sed statement

--- rnaseqpipeline/Install.py
+++ rnaseqpipeline/Install.py
@@ -220,7 +220,7 @@ class Install():
             sp.call('cd {}/RepeatModeler-open-1.0.11;cp ../RepeatModeler_CONFIG RepModelConfig.pm'.format(options.install_dir),
                     shell = True)
 
-            sp.call('sed -i "s,#!/usr/bin/perl,$(which perl),g" {}/RepeatModeler-open-1.0.11/RepeatModeler'.format(options.install_dir),
+            sp.call('sed -i "s,\#\!/u1/local/bin/perl,$(which perl),g" {}/RepeatModeler-open-1.0.11/RepeatModeler'.format(options.install_dir),
                     shell = True) # replace the perl shebang line
 
             sp.call("echo \'# RepeatModeler installation dir\' >> ~/.bashrc; echo \'export PATH={}/RepeatModeler-open-1.0.11:$PATH\' >> ~/.bashrc".format(
