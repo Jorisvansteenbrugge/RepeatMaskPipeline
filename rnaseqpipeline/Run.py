b043c5b009c14c025ed908fdc0a89bf2b5b89908
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-19 14:47:55 +0200

    test

--- rnaseqpipeline/Run.py
+++ rnaseqpipeline/Run.py
@@ -8,8 +8,8 @@ class Run():
 
 
 import subprocess as sp
-out_file = open("{}/out.log".format(options.install_dir), 'w') # logging standard output
-err_file = open("{}/err.log".format(options.install_dir), 'w') # Logging standeard error
+# out_file = open("{}/out.log".format(options.install_dir), 'w') # logging standard output
+# err_file = open("{}/err.log".format(options.install_dir), 'w') # Logging standeard error
 
 def call_sp(command):
     sp.call(command, shell = True)#, stdout = out_file, stderr = err_file)
