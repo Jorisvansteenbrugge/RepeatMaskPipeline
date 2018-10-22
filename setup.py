9cfa54000504e8d5795faac227141457c4157d7f
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-22 13:26:40 +0200

    Adding fastaSplitters

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
         version = "0.1",
         description = 'WUR nematology rnaseq pipeline',
         packages = ['rnaseqpipeline'],
-        scripts = ['scripts/rnaseqpipeline']
+        scripts = ['scripts/rnaseqpipeline', 'scripts/fastaSplitter']
         )
