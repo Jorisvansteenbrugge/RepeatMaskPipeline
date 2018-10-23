ff5025059da89e78dbd5a7c0c92c617fb3c1c060
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-23 09:47:47 +0200

    added package metadata

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from os import path
 setup(
         name = "rnaseqpipeline",
         version = "0.1",
+        author = "Joris van Steenbrugge",
+        author_email = "joris.vansteenbrugge@wur.nl",
+        packages = find_packages(),
         description = 'WUR nematology rnaseq pipeline',
         packages = ['rnaseqpipeline'],
         scripts = ['scripts/rnaseqpipeline', 'scripts/fastaSplitter']
