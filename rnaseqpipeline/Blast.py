0c50760e0f1b97f3a2a4b7290622997f419397ed
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-23 11:46:02 +0200

    wip

--- rnaseqpipeline/Blast.py
+++ rnaseqpipeline/Blast.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
-import subprocess as sp
+
 from Bio import SeqIO
 from joblib import Parallel, delayed
+import multiprocessing
+import subprocess as sp
+import random
+import time
+
 
 class Blaster():
 
-    def blastFasta(fasta_file, blast_type):
+    def blastFasta(fasta_file, blast_type, n_threads, database = 'nr', remote = '-remote'):
         records = list(SeqIO.parse(fasta_file, 'fasta'))
 
-        blast(records[0], blast_type, database = 'swissprot')
+        results = Parallel(n_jobs = n_threads)(delayed(blast(i, blast_type, database)) (i) for i in records)
+        print(results)
 
 
 
-def blast(record, blast_type, database = 'nr', format_type = "Text", remote = "-remote"):
+def blast(record, blast_type, database = 'nr', remote = "-remote"):
 
     blast_cmd = "{0} -db {1} {2} -query - ".format(blast_type, database, remote)
 
     p = sp.Popen(blast_cmd, stdin = sp.PIPE, stdout = sp.PIPE, shell = True)
     blast_out = p.communicate(input=str(record.seq).encode())
-    print(blast_out)
+    return blast_out
