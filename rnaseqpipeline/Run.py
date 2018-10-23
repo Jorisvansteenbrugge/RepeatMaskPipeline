f808f5f769deea5c1f18f10983c97aed3820f9bf
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-23 16:42:15 +0200

    wip

--- rnaseqpipeline/Run.py
+++ rnaseqpipeline/Run.py
@@ -5,7 +5,7 @@ class Run():
     def run_all(options):
 
 
-        sequence = [RepeatModeler, blastPrep]#, blastNR]
+        sequence = [RepeatModeler, blastPrep, blastNR]
         entry_point = lookup_progress(options)
 
         for i in range(entry_point, len(sequence)):
