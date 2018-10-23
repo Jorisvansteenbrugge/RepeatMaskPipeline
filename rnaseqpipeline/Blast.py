80fd54b5aa3d67f3f3a332474bfb4ff028656b9a
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-10-23 11:30:12 +0200

    wip

--- rnaseqpipeline/Blast.py
+++ rnaseqpipeline/Blast.py
@@ -14,7 +14,7 @@ class Blaster():
 
 def blast(record, blast_type, database = 'nr', format_type = "Text", remote = "-remote"):
 
-    blast_cmd = "{0} -database {1} {2} -query - ".format(blast_type, database, remote)
+    blast_cmd = "{0} -db {1} {2} -query - ".format(blast_type, database, remote)
 
     p = sp.Popen(blast_cmd, stdin = sp.PIPE, stdout = sp.PIPE, shell = True)
     blast_out = p.communicate(input=str(record.seq).encode())
