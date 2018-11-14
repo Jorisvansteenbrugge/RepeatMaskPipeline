a2037abae11641ece4a2e1ab1ca787fb6576ee22
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-11-14 11:02:09 +0100

    docker build

--- docker/install.sh
+++ docker/install.sh
@@ -30,19 +30,22 @@ pip3 install AnnotationPipeline
 
 annotation_pipeline install pipeline --dir /opt -g True
 
-#apt install -y augustus \
-#       	       augustus-data \
-#	       augustus-doc \
-#	       bamtools \
-#	       libbamtools-dev \
-#	       samtools
+apt install -y augustus \
+      	       augustus-data \
+	       augustus-doc \
+	       bamtools \
+	       libbamtools-dev \
+	       samtools
 
-
+# Augustus config path -> for braker2
+echo 'export AUGUSTUS_CONFIG_PATH=/usr/share/augustus/config' >> ~/.bashrc
 
 # Genome Threader -> for braker2
-cd /opt; http://genomethreader.org/distributions/gth-1.7.1-Linux_x86_64-64bit.tar.gz
+cd /opt; wget http://genomethreader.org/distributions/gth-1.7.1-Linux_x86_64-64bit.tar.gz
 cd /opt; tar xf gth-1.7.1-Linux_x86_64-64bit.tar.gz
-echo 'export PATH=$PATH:/opt/gth-1.7.1-Linux_x86_64-64bit/bin >> ~/.bashrc'
-echo 'export BSSMDIR=/opt/gth-1.7.1-Linux_x86_64-64bit/bin/bssm >> ~/.bashrc'
-echo 'export GTHDATADIR=/opt/gth-1.7.1-Linux_x86_64-64bit/bin/gthdata >> ~/.bashrc'
-echo 'export ALIGNMENT_TOOL_PATH=/opt/gth-1.7.1-Linux_x86_64-64bit/ >> ~/.bashrc'
+echo 'export PATH=$PATH:/opt/gth-1.7.1-Linux_x86_64-64bit/bin' >> ~/.bashrc
+echo 'export BSSMDIR=/opt/gth-1.7.1-Linux_x86_64-64bit/bin/bssm' >> ~/.bashrc
+echo 'export GTHDATADIR=/opt/gth-1.7.1-Linux_x86_64-64bit/bin/gthdata' >> ~/.bashrc
+echo 'export ALIGNMENT_TOOL_PATH=/opt/gth-1.7.1-Linux_x86_64-64bit/' >> ~/.bashrc
+
+
