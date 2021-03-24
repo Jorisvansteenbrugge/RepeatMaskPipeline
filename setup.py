c34292809e5c2a899791ec8d2c06f9a88ef9f2c1
Joris <joris.vansteenbrugge@wur.nl>
2021-03-24 08:26:16 +0100

    Update and rename RepeatMaskPipeline/setup.py to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 from os import path
 
 setup(
-        name             = "AnnotationPipeline",
+        name             = "RepeatMaskPipeline",
         version          = "0.18",
         author           = "Joris van Steenbrugge",
         author_email     = "joris.vansteenbrugge@wur.nl",
         packages         = find_packages(),
-        description      = 'WUR nematology Annotation Pipeline',
-        scripts          = ['scripts/annotation_pipeline', 'scripts/fastaSplitter'],
+        description      = 'WUR nematology Repeatmask Pipeline',
+        scripts          = ['scripts/Repeatmask_pipeline', 'scripts/fastaSplitter'],
         install_requires = ['joblib', 'biopython'],
         )
